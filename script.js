fd05a6066ca14bcdeb450bb7c37374c119e3aaad
addinaamiruddin <78295393+addinaamiruddin@users.noreply.github.com>
2023-09-20 19:17:03 +0800

    initial functions and design

---
+++ script.js
@@ -0,0 +1,79 @@
+// JavaScript code for student's dashboard
+
+const assignmentTable = document.getElementById("assignment-table");
+
+// Retrieve assignments from local storage
+const assignments = JSON.parse(localStorage.getItem("assignments")) || [];
+
+// Function to update assignment status based on due date and submission
+function updateAssignmentStatus(assignment) {
+    const currentDate = new Date();
+    const dueDate = new Date(assignment.dueDate);
+
+    if (currentDate > dueDate) {
+        assignment.status = "Late Submission";
+    } else if (assignment.submitted) {
+        assignment.status = "Submitted";
+    } else {
+        assignment.status = "Not Submitted";
+    }
+
+    return assignment.status;
+}
+
+// Function to handle assignment submission
+function submitAssignment(assignment) {
+    // Update the assignment's submission status
+    assignment.submitted = true;
+
+    // Update the assignment status based on due date and submission
+    const status = updateAssignmentStatus(assignment);
+
+    // Save the updated assignment back to local storage
+    localStorage.setItem("assignments", JSON.stringify(assignments));
+
+    // Refresh the table to reflect the updated status
+    displayAssignments();
+}
+
+// Function to display assignments in the table
+function displayAssignments() {
+    const tbody = document.getElementById("assignment-list");
+    tbody.innerHTML = "";
+
+    assignments.forEach((assignment) => {
+        const row = document.createElement("tr");
+        const nameCell = document.createElement("td");
+        const dueDateCell = document.createElement("td");
+        const statusCell = document.createElement("td");
+        const submitCell = document.createElement("td");
+
+        nameCell.textContent = assignment.name;
+        dueDateCell.textContent = assignment.dueDate;
+
+        // Update the assignment status based on due date and submission
+        const status = updateAssignmentStatus(assignment);
+        statusCell.textContent = status;
+
+        // Create a "Submit" button
+        const submitButton = document.createElement("button");
+        submitButton.textContent = "Submit";
+        submitButton.addEventListener("click", () => submitAssignment(assignment));
+
+        submitCell.appendChild(submitButton);
+
+        row.appendChild(nameCell);
+        row.appendChild(dueDateCell);
+        row.appendChild(statusCell);
+        row.appendChild(submitCell);
+
+        tbody.appendChild(row);
+    });
+}
+
+// Initial display of assignments
+displayAssignments();
+
+
+// Initial display of assignments
+displayAssignments();
---
+++ teacher.js
@@ -0,0 +1,74 @@
+// JavaScript code for teacher's dashboard
+
+const assignmentTable = document.getElementById("assignment-table");
+const assignmentList = document.getElementById("assignment-list");
+
+function displayAssignments() {
+    const tbody = assignmentList;
+    tbody.innerHTML = "";
+
+    assignments.forEach((assignment, index) => {
+        const row = document.createElement("tr");
+        const nameCell = document.createElement("td");
+        const dueDateCell = document.createElement("td");
+        const deleteCell = document.createElement("td");
+
+        nameCell.textContent = assignment.name;
+        dueDateCell.textContent = assignment.dueDate;
+
+        // Create a "Delete" button
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.addEventListener("click", () => deleteAssignment(index));
+
+        deleteCell.appendChild(deleteButton);
+
+        row.appendChild(nameCell);
+        row.appendChild(dueDateCell);
+        row.appendChild(deleteCell);
+
+        tbody.appendChild(row);
+    });
+}
+
+function deleteAssignment(index) {
+    if (confirm("Are you sure you want to delete this assignment?")) {
+        assignments.splice(index, 1);
+
+        // Save the updated assignments back to local storage
+        localStorage.setItem("assignments", JSON.stringify(assignments));
+
+        // Refresh the table to reflect the deletion
+        displayAssignments();
+    }
+}
+
+function addAssignment() {
+    const assignmentName = assignmentNameInput.value;
+    const dueDate = dueDateInput.value;
+
+    if (assignmentName && dueDate) {
+        const assignment = {
+            name: assignmentName,
+            dueDate: dueDate,
+        };
+
+        // Store the assignment in local storage
+        const assignments = JSON.parse(localStorage.getItem("assignments")) || [];
+        assignments.push(assignment);
+        localStorage.setItem("assignments", JSON.stringify(assignments));
+
+        // Display the assignment in the table
+        displayAssignments();
+
+        // Clear input fields
+        assignmentNameInput.value = "";
+        dueDateInput.value = "";
+    } else {
+        alert("Please enter both assignment name and due date.");
+    }
+}
+
+// Initial display of assignments
+const assignments = JSON.parse(localStorage.getItem("assignments")) || [];
+displayAssignments();
